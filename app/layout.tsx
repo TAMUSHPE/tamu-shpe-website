b8d09e4845731f68614ce8aa5ab807f752219f38
gtraslav <gjtraslavina@gmail.com>
2024-10-10 17:21:31 -0500

    put the images in a list, now iterate through list

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,7 +30,6 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <Navbar />
-        <Sponsors />
         <main>{children}</main>
       </body>
     </html>
