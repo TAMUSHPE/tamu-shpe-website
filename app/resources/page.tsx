fa5aca92cee02ff66f4687904b9725b8f58b2d32
Axel Fontaine <22axfontaine@gmail.com>
2025-02-18 18:31:21 -0600

    Fixed tailwind dynamic styling

--- app/resources/page.tsx
+++ app/resources/page.tsx
@@ -14,9 +14,6 @@ interface ResourceSectionProps {
 
 function ResourceSection({ title, description, color, image, link_map, place_end }: ResourceSectionProps) {
   const router = useRouter();
-  const border_color = 'border-' + color;
-  const text_color = 'text-' + color;
-  const bg_color = 'bg-' + color;
 
   return (
     <div
@@ -34,10 +31,11 @@ function ResourceSection({ title, description, color, image, link_map, place_end
       <div
         className={`flex flex-col border-[12px] border-t-0 ${
           place_end ? 'md:border-r-0' : 'md:border-l-0'
-        } md:border-t-[12px] ${border_color} w-full h-5/6 p-8 md:min-h-fit min-w-96 justify-between items-start gap-4`}
+        } md:border-t-[12px] w-full h-5/6 p-8 md:min-h-fit min-w-96 justify-between items-start gap-4`}
+        style={{ borderColor: color }}
       >
         <div className="flex flex-col gap-2">
-          <h2 className={`${text_color} text-3xl ${place_end && 'text-end'}`}> {title} </h2>
+          <h2 className={`text-3xl ${place_end && 'text-end'}`} style={{ color: color }}> {title} </h2>
           <p className={`text-[#363636] text-lg ${place_end && 'text-end'}`}>{description}</p>
         </div>
         <div className={`${place_end && 'justify-end'} flex flex-row w-full flex-wrap gap-2`}>
@@ -45,7 +43,8 @@ function ResourceSection({ title, description, color, image, link_map, place_end
             <button
               key={key}
               onClick={() => router.push(link_map[key])}
-              className={`${bg_color} text-white px-5 py-2 font-semibold rounded-md text-lg max-w-72`}
+              className='text-white px-5 py-2 font-semibold rounded-md text-lg max-w-72'
+              style={{ backgroundColor: color }}
             >
               {key}
             </button>
@@ -71,17 +70,17 @@ export default function Resources() {
       <ResourceSection
         title="Tech Affairs Website"
         description="The Tech Affairs Website provides a user guide for both creating a Personal Website as well as learning how to code in Python."
-        color="[#500000]"
+        color="#500000"
         image="/tech_affairs.png"
         link_map={{
-          'Tech Affairs Website': 'https://techaffairs.tamu.edu/',
+          'Tech Affairs Website': 'https://tech.tamushpe.org/',
         }}
       />
       <ResourceSection
         title="Job Opportunities"
         description="Whether it be jobs on campus or a summer internships, opportunities are readily available to those seeking the next step in their career."
         image="/jobs.png"
-        color="orange-700"
+        color="#D33A02"
         link_map={{
           'Technical Opportunities': 'https://careercenter.tamu.edu/',
           JobsForAggies: 'https://employees.tamu.edu/jobsforaggies/',
@@ -94,7 +93,7 @@ export default function Resources() {
         description="Living in a virtual world can take a toll on one's mental health.
            In SHPE, we believe that mental health is an important aspect and should be addressed with utmost importance.
            TAMU Helpline: 979-845-2700 --- About the Helpline"
-        color="blue-500"
+        color="#60a5fa"
         image="/mental_health.png"
         link_map={{
           'Self Help Resources': 'https://uhs.tamu.edu/mental-health/index.html',
