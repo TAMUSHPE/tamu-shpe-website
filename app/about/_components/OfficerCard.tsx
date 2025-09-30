4b5a3803925ab5478ea59fde01710942e741d4c0
leosantosss <lsantos021149@gmail.com>
2025-09-30 18:08:58 -0500

    Officer card info changed

--- app/about/_components/OfficerCard.tsx
+++ app/about/_components/OfficerCard.tsx
@@ -27,7 +27,7 @@ export default function OfficerCard(props: {
   return (
     <div onClick={handleFlip} className="cursor-pointer h-96 w-56 rounded-xl shrink-0" style={{ perspective: '1000px' }}>
       <motion.div
-        className="w-full h-full bg-[#1870B8] shrink-0 rounded-xl "
+        className="w-full h-full bg-[#000000] shrink-0 rounded-xl "
         initial={false}
         animate={{ rotateY: isFlipped ? 180 : 360 }}
         transition={{ duration: 0.5, animationDirection: 'normal' }}
