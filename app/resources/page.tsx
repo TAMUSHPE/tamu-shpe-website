7ffbe610c1758c293a458b139e18192c6cdbc7b0
EmilianodeAlba <2810emiliano@gmail.com>
2026-08-09 21:51:06 -0500

    Color Grading
    Color grading for new theme, as well as possible 4th widget.

--- app/resources/page.tsx
+++ app/resources/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 
 interface ResourceSectionProps {
   title: string;
-  description: string;
+  description: React.ReactNode;
   color: string;
   image: string;
   link_map: Record<string, string>;
@@ -13,7 +13,7 @@ interface ResourceSectionProps {
 function ResourceSection({ title, description, color, image, link_map, place_end }: ResourceSectionProps) {
   return (
     <div
-      className={`flex ${place_end ? 'flex-col-reverse' : 'flex-col'} w-full md:min-h-[50vh] items-center md:flex-row`}
+      className={`flex ${place_end ? 'flex-col-reverse' : 'flex-col'} w-full md:min-h-[50vh] items-center md:flex-row md:px-32`}
     >
       {!place_end && (
         <Image
@@ -27,14 +27,14 @@ function ResourceSection({ title, description, color, image, link_map, place_end
       <div
         className={`flex flex-col border-[12px] border-t-0 ${
           place_end ? 'md:border-r-0' : 'md:border-l-0'
-        } md:border-t-[12px] w-full h-5/6 p-8 md:min-h-fit sm:min-w-96 justify-between items-start gap-4`}
+        } md:border-t-[12px] w-full h-5/6 p-8 md:min-h-[400px] sm:min-w-96 justify-between items-start gap-4`}
         style={{ borderColor: color }}
       >
         <div className="flex flex-col gap-2">
-          <h2 className={`text-2xl sm:text-3xl ${place_end && 'text-end'}`} style={{ color: color }}>
+          <h2 className={`text-2xl sm:text-3xl font-semibold ${place_end && 'text-end'}`} style={{ color: color }}>
             {title}
           </h2>
-          <p className={`text-[#363636] text-md sm:text-lg ${place_end && 'text-end'}`}>{description}</p>
+          <p className={`text-[#363636] text-lg sm:text-xl whitespace-pre-line ${place_end && 'text-end'}`}>{description}</p>
         </div>
         <div className={`${place_end && 'sm:justify-end'} justify-center sm:justify-start flex flex-row w-full flex-wrap gap-2`}>
           {Object.keys(link_map).map((key) => (
@@ -72,14 +72,14 @@ export default function Resources() {
         color="#500000"
         image="/resources/tech_affairs.png"
         link_map={{
-          'Tech Affairs Website': 'https://tech.tamushpe.org/',
+          'Visit Website': 'https://tech.tamushpe.org/',
         }}
       />
       <ResourceSection
-        title="Job Opportunities"
+        title="Professional Resources"
         description="Whether it be jobs on campus or a summer internships, opportunities are readily available to those seeking the next step in their career."
-        image="/resources/jobs.png"
-        color="#D33A02"
+        image="/resources/professionalResources.png"
+        color="#191740"
         link_map={{
           'Technical Opportunities': 'https://careercenter.tamu.edu/',
           JobsForAggies: 'https://employees.tamu.edu/jobsforaggies/',
@@ -89,11 +89,19 @@ export default function Resources() {
       />
       <ResourceSection
         title="Mental Health"
-        description="Living in a virtual world can take a toll on one's mental health.
-           In SHPE, we believe that mental health is an important aspect and should be addressed with utmost importance.
-           TAMU Helpline: 979-845-2700 --- About the Helpline"
-        color="#60a5fa"
-        image="/resources/mental_health.png"
+        description={
+          <>
+            Living in a virtual world can take a toll on one's mental health. In SHPE, we believe that mental health is an important aspect and should be addressed with utmost importance.
+            <br />
+            <br />
+            TAMU Helpline: 979-845-2700 ---{' '}
+            <Link href="https://mentalhealth.tamu.edu/crisis-resources.html" className="underline">
+              About the Helpline
+            </Link>
+          </>
+        }
+        color="#3C0500"
+        image="/resources/mentalHealth.png"
         link_map={{
           'Self Help Resources': 'https://uhs.tamu.edu/mental-health/index.html',
           'Counseling Appointments': 'https://tamuportal.pointnclick.com/',
